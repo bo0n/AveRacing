f011e7e578b2cc1a40d28dad897df218b577d6fe
bo0n <wannatrymybamboo@hotmail.it>
2014-07-21 23:56:01 +0200

    Dioberto.
    Dioberto.

---
+++ src/it/bast/averacing/GameView.java
@@ -0,0 +1,22 @@
+package it.bast.averacing;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.view.View;
+
+public class GameView extends View {
+    private Bitmap bmp;
+    
+    public GameView(Context context) {
+          super(context);
+          bmp = BitmapFactory.decodeResource(getResources(), R.drawable.redcar);
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.BLACK);
+        canvas.drawBitmap(bmp, 10, 10, null);
+    }
+}
